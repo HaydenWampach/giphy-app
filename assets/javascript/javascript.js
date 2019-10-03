eb2a3114b26c6145b01902372dda826d28722b7d
Hayden Wampach <wampachhayden@gmail.com>
2019-10-02 23:00:16 -0700

    added corrections for syntax, added font style, limited amount of gifs to 10

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,78 +1,120 @@
 $(document).ready(function() {
-    //array of topics
-    var topics = ['Golf', 'Tiger Woods', 'Baseball', 'Aaron Judge', 'Football', 'Russel Wilson', 'Soccer', 'Lionel Messi', 'Snowboarding', 'Travis Rice', 'Swimming', 'Michael Phelps', 'Basketball', 'Michael Jordan', 'Boxing', 'Mike Tyson'];
-    function createHtmlButton() {
-        for  (let i = 0; i < topics.length; i++) {
-            var a = $('<button>');
-            //add classes and attributes to each button from array
-            a.addClass('btn btn-primary bnt-lg');
-            a.attr('data-name', topics[i]);
-            a.attr('id', 'topicButtons');
-            a.text(topics[i]);
-            //append each button from array to the gifButtonArray div
-            $('.gifButtonArray').append(a);
-            $('#center-text-box').empty();
-        }
-    }
-    createHtmlButton();
+	//array of topics
+	var topics = [
+		"Golf",
+		"Tiger Woods",
+		"Baseball",
+		"Aaron Judge",
+		"Football",
+		"Russel Wilson",
+		"Soccer",
+		"Lionel Messi",
+		"Snowboarding",
+		"Travis Rice",
+		"Swimming",
+		"Michael Phelps",
+		"Basketball",
+		"Michael Jordan",
+		"Boxing",
+		"Mike Tyson",
+	];
+	function createHtmlButtons() {
+		for (let i = 0; i < topics.length; i++) {
+			// $('.gifButtonArray').empty();
 
-    $(document).on('click', '#toppicButtons', function (event) {
-        $('#results').empty();
-        var searchInput = $(this).attr("data-name");
-        // api key used to queary the giphy url
-        var apiKey = 'db0649444d680273f8f9737b1fd8b79d';
-        //queary url
-        var quearyUrl = 'https://api.giphy.com/v1/gifs/search?q=' + searchInput + '&limit=10&api_key=' + apiKey;
+			var a = $("<button>");
 
-        $a.jax ({
-            url: quearyUrl,
-            method: "GET"
-        }).then(function(response) {
-            //pan through each queary and select data
-            for (let i = 0; i < response.data.length; i++) {
-                //builds a bootstrap card and assigns it to a variable
-                var rating = reponse.data[i].rating;
-                var data = response.data[i].images;
-                var p = $('<p>').css('font-family','"Poppins", sans-serif').text('Rating: ' + rating);
-                var animated = data.fixed_height_still.url;
-                var image = $('<img class="card-img-top">')
-                    .attr('src', still)
-                    .attr('data-still', still)
-                    .attr('data-animated', animated)
-                    .attr('data-stage', 'still')
-                    .addClass('searchImage');
+			//add classes and attributes to each button from array
 
-                var searchDiv = $('<div class="card" stle="width: 300px;">')
-                    .append(image)
-                    .append(p);
+			a.addClass("btn btn-primary btn-lg");
 
-                $('#results').prepend(searchDiv);
+			a.attr("data-name", topics[i]);
 
-            }
-        })
-    })
-    //function to add input by user into a button
-    $('searchButton').on('click', function(event) {
-        $('.gifButtonArray').empty();
-            event.preventDefault();
-            //pulls text from input box
-            var newCategory = $('#enter-text-box').val().trim();
-            //adds user input to topics array
-            topics.push(newCategory);
+			a.attr("id", "topicButtons");
 
-            //generates button user input
-            createHtmlButtons();
-            return false;
-    })
+			a.text(topics[i]);
 
-    //determines behavior on click of each gif
-    $(document).on('click', '.searchImage', function(){
-        var state = $(this).attr('data-state');
-        if (state == 'still') {
-            $(this).attr('src', $(this).data('animated'));
-        } else {
-            $(this).attr('src', $(this).data('still'));
-            $(this).attr('data-state', 'still');
-        }
-    })
-})
+			//append each button from array to the gifButtonArray div
+
+			$(".gifButtonArray").append(a);
+
+			$("#enter-text-box").empty();
+		}
+	}
+
+	createHtmlButtons();
+
+	// TODO: if button clicked for certain GIF type, display 10 GIFs for that category
+
+	$(document).on("click", "#topicButtons", function(event) {
+		$("#results").empty();
+		var searchInput = $(this).attr("data-name");
+		// api key used to query the giphy url
+		var apiKey = "Sa76H1kYiJwFnZVm1ICYt8jFr8ukfLKj";
+		//query url with apikey, topics array, limit, and rating parameters set
+
+		var queryUrl =
+			"https://api.giphy.com/v1/gifs/search?q=" +
+			searchInput +
+			"&limit=10&api_key=" +
+			apiKey;
+
+		//query the db using above query url
+		$.ajax({
+			url: queryUrl,
+			method: "GET",
+		}).then(function(response) {
+			//pan through each query and select data
+			for (let i = 0; i < response.data.length; i++) {
+				//builds a bootstrap card and assigns it to a variable
+				var rating = response.data[i].rating;
+				var data = response.data[i].images;
+				var p = $("<p>")
+					.css("font-family", '"Poppins", sans-serif')
+                    .text("Rating: " + rating);
+                    
+				var animated = data.fixed_height.url;
+				var still = data.fixed_height_still.url;
+				//build bootstrap cards for each image, assign individual still and animated properties
+				var image = $('<img class="card-img-top">')
+					.attr("src", still)
+					.attr("data-still", still)
+					.attr("data-animated", animated)
+					.attr("data-state", "still")
+					.addClass("searchImage");
+				var searchDiv = $('<div class="card" style="width: 300px;">')
+					.append(image)
+					.append(p);
+				$("#results").prepend(searchDiv);
+			}
+		});
+	});
+
+	//function to add input by user into a button
+	$("#searchButton").on("click", function(event) {
+		$(".gifButtonArray").empty();
+		event.preventDefault();
+		//pulls text from input box
+		var newCategory = $("#enter-text-box")
+			.val()
+			.trim();
+		//adds user input to topics array
+		topics.push(newCategory);
+		//generates button user input
+		createHtmlButtons();
+		return false;
+	});
+	//determines behavior on click of each gif
+	$(document).on("click", ".searchImage", function() {
+		var state = $(this).attr("data-state");
+		if (state == "still") {
+			//animates gif if click while still
+			$(this).attr("src", $(this).data("animated"));
+			$(this).attr("data-state", "animated");
+		} else {
+			//makes gif stop animation
+			$(this).attr("src", $(this).data("still"));
+			$(this).attr("data-state", "still");
+		}
+	});
+});
